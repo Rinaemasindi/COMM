6c43d7575b5e443362c479fb72f7f5e02270a590
Rinae <Rinaemasindi@outlook.com>
2023-04-20 04:40:14 +0200

    fixed post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function store(Request $request){
         $this->validate($request,[
             'body'=> 'required',
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,36 +15,32 @@
                     </button>
                 </form>
             </div>
-    </div>
+        </div>
     @if ($posts->count())
         @foreach ($posts as $post)
-            
+            {{-- Posts start here --}}
+                <div class="px-5 mt-5 py-4 bg-white shadow rounded-lg max-w-lg">
+                    <div class="flex mb-4">
+                        <div class="ml-2 mt-0.5">
+                            <span class="block font-medium text-base leading-snug text-black">{{$post->user->name}}</span>
+                            <span class="block text-sm text-gray-500 font-light leading-snug">{{$post->created_at}}</span>
+                        </div>
+                    </div>
+                    <p class="text-gray-800 leading-snug md:leading-normal">{{$post->body}}</p>
+                    <div class="flex justify-between items-center mt-5">
+                        <div class="flex ">
+
+                            <a href="#" class="text-gray-500 mr-1">Like</a> <span
+                                class="ml-1 text-gray-500 font-light">8</span>
+                        </div>
+                        <div class="ml-1 text-gray-500 font-light">33 comments</div>
+                    </div>
+                </div>
+            {{-- post ends here --}}
+
         @endforeach
     @else
         
     @endif
-        {{-- Posts start here --}}
-        <div class="px-5 mt-5 py-4 bg-white shadow rounded-lg max-w-lg">
-            <div class="flex mb-4">
-                <div class="ml-2 mt-0.5">
-                    <span class="block font-medium text-base leading-snug text-black">Loyce Kuvalis</span>
-                    <span class="block text-sm text-gray-500 font-light leading-snug">16 December at 08:25</span>
-                </div>
-            </div>
-            <p class="text-gray-800 leading-snug md:leading-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <div class="flex justify-between items-center mt-5">
-                <div class="flex ">
-
-                    <a href="#" class="text-gray-500 mr-1">Like</a> <span
-                        class="ml-1 text-gray-500 font-light">8</span>
-                </div>
-                <div class="ml-1 text-gray-500 font-light">33 comments</div>
-            </div>
-        </div>
     </div>
-    {{-- post ends here --}}
 @endsection
